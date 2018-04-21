6512648b6abb52c9bede5a0ec7d935bdcc46d5d9
dknore <darcy.knore@gmail.com>
2018-04-21 10:57:43 -0700

    initial scaffolding

---
+++ lab-27-darcy/webpack.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /\.css$/,loader: ['style-loader', 'css-loader']}
+    ]
+  }
+};
+
+module.exports = config;
